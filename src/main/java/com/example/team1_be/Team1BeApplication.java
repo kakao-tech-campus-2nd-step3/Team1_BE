68faa3645ff2242b9fffdd8062a05d80101b27ed
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-09-13 17:34:06 +0900

    feat: init

---
+++ src/main/java/com/example/team1_be/Team1BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.team1_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team1BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team1_be/Team1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team1_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
