12b277ca523416ca8b2bfad31b8f02b026ab7891
sunandrabbit <sunandrabbit@gamil.com>
2024-09-20 12:51:21 +0900

    feat : Project Repository 구현

---
+++ src/main/java/com/example/team1_be/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.example.team1_be.repository;
+
+import com.example.team1_be.entity.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Page<Project> findAll(Pageable pageable);
+}
