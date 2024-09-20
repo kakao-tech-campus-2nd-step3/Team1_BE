200750f475b7b036690fea3ae079b2cfafb74cb2
sunandrabbit <sunandrabbit@gamil.com>
2024-09-20 15:52:43 +0900

    fix : annotation 수정

--- src/main/java/com/example/team1_be/Team1BeApplication.java
+++ src/main/java/com/example/team1_be/Team1BeApplication.java
@@ -2,8 +2,10 @@ package com.example.team1_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Team1BeApplication {
 
     public static void main(String[] args) {
