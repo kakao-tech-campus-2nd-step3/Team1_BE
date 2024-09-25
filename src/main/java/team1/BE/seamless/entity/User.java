989084cbc3b9916482966dc61f1e86eb0edc3243
sunandrabbit <sunandrabbit@gamil.com>
2024-09-25 21:18:31 +0900

    fix : table 이름이 예약어여서 다른 이름으로 수정

--- src/main/java/team1/BE/seamless/entity/User.java
+++ src/main/java/team1/BE/seamless/entity/User.java
@@ -10,7 +10,7 @@ import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity(name = "user")
+@Entity(name = "user_table")
 public class User extends BaseEntity{
 
     public User() {
