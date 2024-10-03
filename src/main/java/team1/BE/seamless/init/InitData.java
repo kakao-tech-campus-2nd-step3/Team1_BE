e404492373d009fa37b7fbe234a6770b1a1b7f57
sunandrabbit <sunandrabbit@gamil.com>
2024-10-04 03:12:30 +0900

    feat : 프로젝트 데이터 추가

--- src/main/java/team1/BE/seamless/init/InitData.java
+++ src/main/java/team1/BE/seamless/init/InitData.java
@@ -10,15 +10,18 @@ import org.springframework.stereotype.Component;
 public class InitData {
 
     private final TestCreator testCreator;
+    private final ProjectCreator projectCreator;
 
     @Autowired
-    public InitData(TestCreator testCreator) {
+    public InitData(TestCreator testCreator, ProjectCreator projectCreator) {
         this.testCreator = testCreator;
+        this.projectCreator = projectCreator;
     }
 
     @PostConstruct
     public void init() {
         //        테스트 생성
         testCreator.creator();
+//        projectCreator.creator();
     }
 }
