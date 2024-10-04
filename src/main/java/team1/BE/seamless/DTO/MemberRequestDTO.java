4ab415e891781ac0acdc332c1329151221536873
suno-boy <tnsgh3691@gmail.com>
2024-10-04 20:50:21 +0900

    feat: 유효성 검증 포함한 Member요청 DTO 구현

--- src/main/java/team1/BE/seamless/DTO/MemberRequestDTO.java
+++ src/main/java/team1/BE/seamless/DTO/MemberRequestDTO.java
@@ -1,5 +1,67 @@
 package team1.BE.seamless.DTO;
 
-public class MemberRequestDTO {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import team1.BE.seamless.util.page.PageParam;
 
+public class MemberRequestDTO {
+    public static class getMemberList extends PageParam {
+
+    }
+
+    @NotBlank(message = "이름은 필수 입력 사항입니다.")
+    @Size(max = 15, message = "이름은 공백 포함 최대 15글자까지 가능합니다.")
+    private String name;
+
+    @NotBlank(message = "역할은 필수 입력 사항입니다.")
+    @Size(max = 15, message = "역할은 공백 포함 최대 15글자까지 가능합니다.")
+    private String role;
+
+    @Email(message = "유효한 이메일 주소를 입력해주세요.")
+    @NotBlank(message = "이메일은 필수 입력 사항입니다.")
+    private String email;
+
+    private String imageURL;
+
+    public MemberRequestDTO() {}
+
+    public MemberRequestDTO(String name, String role, String email, String imageURL) {
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.imageURL = imageURL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
