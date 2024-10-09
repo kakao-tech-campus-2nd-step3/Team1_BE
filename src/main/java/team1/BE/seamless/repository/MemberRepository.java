5055e7fe3095ab7cf156899ae32a409966791d00
suno-boy <tnsgh3691@gmail.com>
2024-10-09 10:41:47 +0900

    refactor: 필요없는 메서드 삭제

--- src/main/java/team1/BE/seamless/repository/MemberRepository.java
+++ src/main/java/team1/BE/seamless/repository/MemberRepository.java
@@ -11,8 +11,6 @@ import team1.BE.seamless.entity.MemberEntity;
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
-    Optional<MemberEntity> findByIdAndProjectEntityIdAndIsDeleteFalse(Long id, Long projectId);
-
     Page<MemberEntity> findAllByProjectEntityIdAndIsDeleteFalse(Long projectId, Pageable pageable);
 
     Optional<MemberEntity> findByProjectEntityIdAndEmailAndIsDeleteFalse(Long projectId, String email);
