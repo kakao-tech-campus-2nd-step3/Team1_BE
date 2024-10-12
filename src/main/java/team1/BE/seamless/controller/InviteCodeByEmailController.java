ea1be2a890456bab1639b62286905f6b6768effa
suno-boy <tnsgh3691@gmail.com>
2024-10-12 16:19:57 +0900

    fix: 초대코드 컨트롤러에서 에러처리 BaseHandler로 변경

--- src/main/java/team1/BE/seamless/controller/InviteCodeByEmailController.java
+++ src/main/java/team1/BE/seamless/controller/InviteCodeByEmailController.java
@@ -5,12 +5,14 @@ package team1.BE.seamless.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team1.BE.seamless.DTO.InviteRequestDTO;
 import team1.BE.seamless.service.InviteCodeByEmailService;
+import team1.BE.seamless.util.errorException.BaseHandler;
 import team1.BE.seamless.util.page.SingleResult;
 
 @Tag(name = "이메일 전송")
@@ -27,11 +29,11 @@ public class InviteCodeByEmailController {
         try {
             String message =
                 "You have been invited to join the project with ID: " + inviteRequest.getProjectId()
-                    + "\nAnd Participation code: ";
-            inviteService.sendProjectInvite(inviteRequest.getEmail(), message);
-            return new SingleResult<>("프로젝트 초대 요청이 성공적으로 처리되었습니다.");
+                    + "\nAnd Participation code: 참여코드 들어가야함";
+            inviteService.sendProjectInvite(inviteRequest.getEmail(), message, inviteRequest.getProjectId());
+            return new SingleResult<>("이메일로 프로젝트 초대코드 전송이 성공적으로 처리되었습니다.");
         } catch (Exception e) {
-            return new SingleResult<>("이메일 초대 실패: " + e.getMessage());
+            throw new BaseHandler(HttpStatus.BAD_REQUEST,"이메일로 프로젝트 초대코드 전송이 실패되었습니다. : " + e.getMessage());
         }
     }
 }
